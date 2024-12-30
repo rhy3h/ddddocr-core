0dd50fd6dfab1787524965ae5a79d477309aae0f
rhy3h <rhy3htw@gmail.com>
2024-12-30 23:52:20 +0800

    feat: initialize base constructor

---
+++ index.js
@@ -0,0 +1,8 @@
+const { DetectionBase } = require('./DetectionBase');
+const { CHARSET_RANGE, OCRBase } = require('./OcrBase');
+
+module.exports = {
+    CHARSET_RANGE,
+    OCRBase,
+    DetectionBase
+}
