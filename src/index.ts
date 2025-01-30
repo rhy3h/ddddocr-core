9cbcbaab3faf88dcb4318e6381b7cb54fce99967
rhy3h <rhy3htw@gmail.com>
2025-01-30 23:56:05 +0800

    refactor: Transfer to typescript

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { DetectionBase } from './DetectionBase';
+import { CHARSET_RANGE, OCRBase } from './OcrBase';
+import { drawRectangle } from './utils/image-utils';
+
+export {
+    CHARSET_RANGE,
+    OCRBase,
+    DetectionBase,
+    drawRectangle
+};
